fb47dab76738184f2d9977052ac276e89a4ab033
carlos <carlos.guimaraes10@etec.sp.gov.br>
2024-08-05 21:30:03 -0300

    first commit

---
+++ src/app/primeiro-projeto/primeiro-projeto.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core"
+@Component({
+    selector:'meu-primeiro-component',
+    standalone: true,
+    imports:[],
+    template:`
+    <h1>Meu Primeiro Componente</h1>
+    `,
+    styles:[]
+})
+
+export class MeuPrimeiroComponent {}
---
+++ src/app/segundo-projeto/segundo-projeto.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core"
+@Component({
+    selector:'meu-segundo-component',
+    standalone: true,
+    imports:[],
+    template:`
+    <h1>Meu Segundo Componente</h1>
+    `
+})
+
+export class MeuSegundoComponent {}
